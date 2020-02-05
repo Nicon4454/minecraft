8d6e4eb16f9412ac07c8cebb4b944a23b8ff2c9f
nicon4454 <dan@webtrooper.com>
2020-02-05 08:30:55 -0500

    First commit

---
+++ lua/rc.lua
@@ -0,0 +1,35 @@
+--[[
+###############################
+## Reactor Control Program   ##
+## by nicon4454              ##
+## 02/05/2020 v0.01          ##
+###############################
+]]
+
+-- Please change this section tp match your needs
+cb = peripheral.wrap("capacitor_bank_0")
+br = peripheral.wrap("BigReactors-Reactor_0")
+mon = peripheral.wrap("monitor_0")
+
+
+-- DO NOT CHANGE ANYTHING BELOW THIS LINE --
+
+
+
+
+
+
+
+
+function comma_value(amount)
+    local formatted = amount
+    while true do
+        formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+        if (k == 0) then break end
+    end
+    return formatted
+end
+
+es = (comma_value(string.format("%.1d", (cb.getEnergyStored()))))
+mes = (comma_value(string.format("%.1d", (cb.getMaxEnergyStored()))))
+print("There is " .. es .. " out of a Max of " .. mes)
