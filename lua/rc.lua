7cf3135f1bff7c7a0e8f7a75be03b8195c14c48a
nicon4454 <dan@webtrooper.com>
2020-02-06 09:26:10 -0500

    getting the constraints in

--- lua/rc.lua
+++ lua/rc.lua
@@ -6,17 +6,24 @@
 ###############################
 ]]
 
--- Please change this section tp match your needs
+-- Please change this section to match your needs
 cb = peripheral.wrap("capacitor_bank_0")
 br = peripheral.wrap("BigReactors-Reactor_0")
 mon = peripheral.wrap("monitor_0")
 
-
+-- the ron is the percent that the reactor will turn on when the power reaches that % of max power in your capacitor bank(s)
+ron = 0.25
+-- the roff is the percent that the reactor will turn off when the power reaches that % of max power in your capacitor bank(s)
+roff = 0.95
 -- DO NOT CHANGE ANYTHING BELOW THIS LINE --
+ch = 0 --case heat
 
 
--- 
 
+
+
+
+function 
 
 function comma_value(amount)
     local formatted = amount
