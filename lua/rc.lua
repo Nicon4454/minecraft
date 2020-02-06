a04a1835743c335df78def62d977d43d34f4812c
Dan <dan@webtrooper.com>
2020-02-06 10:24:39 -0500

    added funtion to det the reactors on/off precents

--- lua/rc.lua
+++ lua/rc.lua
@@ -11,15 +11,17 @@ cb = peripheral.wrap("capacitor_bank_0")
 br = peripheral.wrap("BigReactors-Reactor_0")
 mon = peripheral.wrap("monitor_0")
 
--- the ron is the percent that the reactor will turn on when the power reaches that % of max power in your capacitor bank(s)
-ron = 0.25
--- the roff is the percent that the reactor will turn off when the power reaches that % of max power in your capacitor bank(s)
-roff = 0.95
+-- the ronp is the percent that the reactor will turn on when the power reaches that % of max power in your capacitor bank(s)
+ronp = 0.25
+-- the roffp is the percent that the reactor will turn off when the power reaches that % of max power in your capacitor bank(s)
+roffp = 0.95
 -- DO NOT CHANGE ANYTHING BELOW THIS LINE --
 ch = 0 --case heat
 fh = 0 --Fuel heat
 fl = 0 --Fuel level
 wl = 0 --waste level
+ron = 0
+roff = 0
 active = 0 --weather the reactor is on (1) or off (0)
 
 
@@ -27,7 +29,10 @@ active = 0 --weather the reactor is on (1) or off (0)
 
 
 
-function 
+function reactorohoff()
+    ron = (cb.getMaxEnergyStored() * ronp)
+    roff = (cb.getMaxEnergyStored() * roffp)
+end
 
 function comma_value(amount)
     local formatted = amount
