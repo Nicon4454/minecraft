a5c58ba4b0c997a6895c7d2454ce5c06d3d3aadd
Nicon4454 <Nicon4454@users.noreply.github.com>
2023-04-06 11:35:36 -0400

    Create flux_dust.js

---
+++ kubejs/flux_dust.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event => {
+  event.shaped("64x fluxnetworks:flux_dust", [
+    "RRR", 
+    "RDR", 
+    "RRR"
+  ], {
+    R: "minecraft:redstone",
+    D: "minecraft:diamond",
+  });
+})
