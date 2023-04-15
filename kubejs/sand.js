cc9f28bd7573708785111061e2c1d32a465f3e40
Nicon4454 <Nicon4454@users.noreply.github.com>
2023-04-15 04:23:16 -0400

    Add files via upload

---
+++ kubejs/sand.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event => {
+    event.shaped("8x minecraft:glass", [
+      "SSS", 
+      "SFS", 
+      "SSS"
+    ], {
+      S: "minecraft:sand",
+      F: "mysticalagriculture:fire_essence",
+    });
+  })
